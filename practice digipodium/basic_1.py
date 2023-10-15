979838aa7b64c8ad101df48fa03c458728b1d4c3
bhupendra <bhupendra.adhikari24@gmail.com>
2023-10-15 21:32:21 +0530

    update

---
+++ practice digipodium/basic_1.py
@@ -0,0 +1,12 @@
+from turtle import*
+
+speed('slow')
+bgcolor('red')
+pensize()
+
+for i in range (12):
+    fd(60)
+    lt(30)
+
+hideturtle()
+mainloop()    
---
+++ practice digipodium/turtle_practicequestion.py
@@ -0,0 +1,8 @@
+from turtle import*
+
+for i in range (6):
+    fd(80)
+    lt(60)
+    
+hideturtle()
+mainloop()       
